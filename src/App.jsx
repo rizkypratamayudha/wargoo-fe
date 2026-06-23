7e3a6ec71e4422f68946214031f2c3cf9ee031d2
rizkypratamayudha <masrizki87@gmail.com>
2026-06-23 21:52:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      
+    </BrowserRouter>
+  );
+}
+
+export default App;
